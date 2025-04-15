e0075f1e0d1712fee847fdb88ad5f57c8bc67c87
hanane <h_lahssini@etu.enset-media.ac.ma>
2025-04-15 22:18:23 +0100

    V3

--- patient-ms/src/main/java/com/clinic/patientms/PatientMsApplication.java
+++ patient-ms/src/main/java/com/clinic/patientms/PatientMsApplication.java
@@ -1,13 +1,48 @@
 package com.clinic.patientms;
 
+import com.clinic.patientms.entities.Patient;
+import com.clinic.patientms.enums.Gender;
+import com.clinic.patientms.repositories.RepositoryPatient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class PatientMsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatientMsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMsApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(RepositoryPatient repositoryPatient) {
+        return args -> {
+            Stream.of(
+                    new Object[]{"Mohammed","Kbiri" , Gender.MALE},
+                    new Object[]{"Hanane", "Lahssini",Gender.FEMALE},
+                    new Object[]{"Janat","Fathi", Gender.FEMALE}
+            ).forEach(data -> {
+                String firstName = (String) data[0];
+                String lastName = (String) data[1];
+                Gender gender = (Gender) data[2];
+
+
+                Patient patient = Patient.builder()
+                        .firstName(firstName)
+                        .lastName(lastName)
+                        .email(firstName.toLowerCase() + "@example.com")
+                        .phone("0666666666")
+                        .dateOfBirth(new Date())
+                        .gender(gender)
+                        .build();
+
+                repositoryPatient.save(patient);
+                System.out.println("Inserted patient: " + firstName);
+            });
+        };
+    }
 
 }
---
+++ patient-ms/src/main/java/com/clinic/patientms/entities/Patient.java
@@ -0,0 +1,21 @@
+package com.clinic.patientms.entities;
+import com.clinic.patientms.enums.Gender;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity @Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private Date dateOfBirth;
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+}
